63500f8400d713a2aec05a46eb7d93f8faa62d83
Ceferov88 <ceferov_hafiz@yahoo.com>
2021-07-29 21:05:53 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+variable "vpcname" {
+    type = "string"
+    default = "myvpc"
+}
+
+variable "sshport" {
+    type = number
+    default = 22
+}
+
+variable "anabled" {
+    default = true
+}
+
+variable "mylist" {
+    type = list (string)
+    default = ["Value1","Value2"]
+}
+
+variable "mymap" {
+    type = map 
+    default = {
+        Key1 = "Value1"
+        Key2 = "Value2"
+    }
+}
+#
